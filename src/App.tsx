4e5e3f2e33a7b95ac64fae618059cbfd13daa1aa
jn_xyp <jinan.xyp@gmail.com>
2025-10-06 10:59:06 -0500

    read apikey from env

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const apiKey = process.env.REACT_APP_TMDB_APIKEY;
+const token = process.env.REACT_APP_TMDB_API_TOKEN;
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +21,10 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <p>TMDB API Key: {apiKey}</p>
+          <p>TMDB Token: {token}</p>
+        </div>
       </header>
     </div>
   );
